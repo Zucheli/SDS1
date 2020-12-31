f80d60e8d6188bf034cfb53575eb04afbc7f1c97
Mateus Zucheli <mateuszucheli@hotmail.com>
2020-12-31 10:36:35 -0300

    Adicionando Rotas no React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { AppLoading } from 'expo';
 import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
 import Header from './src/components/Header';
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
 
@@ -18,9 +18,8 @@ export default function App() {
   } else {
     return (
       <View style={styles.container}>
-        <Header />
-        <Home />
-        <StatusBar style="dark" />
+        <Routes />
+        <StatusBar style="light" />
       </View>
     );
   }
@@ -28,7 +27,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#0B1F34'
+    flex: 1
   }
 });
--- front-mobile/src/pages/Home/index.tsx
+++ front-mobile/src/pages/Home/index.tsx
@@ -2,89 +2,94 @@ import React from 'react';
 import { FontAwesome5 as Icon } from '@expo/vector-icons'
 import { Text, StyleSheet, View, Image, Alert } from 'react-native'; 
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../../components/Header';
+import { useNavigation } from '@react-navigation/native'
 
 const Home = () => {
 
-    const handleOnPress = () => {
-        
-    }
+  const navigation = useNavigation();
 
-    return(
-        <>
-            <View style={styles.container}>
-                <Image 
-                    source={require('../../assets/gamer.png')}
-                    style={styles.gamerImage}
-                />
-                <Text style={styles.title}>Vote!</Text>
-                <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
-            </View>
-            <View style={styles.footer}> 
-                <RectButton style={styles.button} onPress={handleOnPress}>
-                    <Text style={styles.buttonText}>
-                        VOTAR AGORA
-                    </Text>
-                    <View  style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="chevron-right" color="#fff" size={25}/>
-                        </Text>
-                    </View>
-                </RectButton>
-            </View>
-        </>
-    )
+  const handleOnPress = () => {
+      navigation.navigate('CreateRecord');
+  }
+
+  return(
+    <>
+      <Header />
+      <View style={styles.container}>
+          <Image 
+              source={require('../../assets/gamer.png')}
+              style={styles.gamerImage}
+          />
+          <Text style={styles.title}>Vote!</Text>
+          <Text style={styles.subTitle}>Nos diga qual é seu jogo favorito!</Text>
+      </View>
+      <View style={styles.footer}> 
+          <RectButton style={styles.button} onPress={handleOnPress}>
+              <Text style={styles.buttonText}>
+                  VOTAR AGORA
+              </Text>
+              <View  style={styles.buttonIcon}>
+                  <Text>
+                      <Icon name="chevron-right" color="#fff" size={25}/>
+                  </Text>
+              </View>
+          </RectButton>
+      </View>
+    </>
+  )
 };
 
 const styles = StyleSheet.create({
-    container: {
-      marginTop:  '20%',
-      backgroundColor: '#0B1F34',
-      alignItems: 'center',
-    },
-    gamerImage: {
-      width: 309,
-      height: 288
-    },
-    title: {
-      color: '#00D4FF',
-      fontSize: 50,
-      fontWeight: 'bold',
-      marginTop: 35,
-      fontFamily: "Play_700Bold",
-    },
-    subTitle: {
-      color: '#ED7947',
-      fontSize: 20,
-      marginTop: 20,
-      fontFamily: "Play_400Regular",
-    },
-    footer: {
-      marginTop: '20%',
-      alignItems: 'center'
-    },
-    button: {
-      backgroundColor: '#00D4FF',
-      flexDirection: 'row',
-      borderRadius: 20
-    },
-    buttonIcon: {
-      backgroundColor: '#ED7947',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: 50,
-      borderBottomRightRadius: 20,
-      borderTopRightRadius: 20
-    },
-    buttonText: {
-      paddingTop: 15,
-      paddingBottom: 15,
-      paddingLeft: 50,
-      paddingRight: 50,
-      fontFamily: "Play_700Bold",
-      fontWeight: 'bold',
-      fontSize: 18,
-      color: '#0B1F34',
-    }
-  });
+  container: {
+    marginTop:  '20%',
+    backgroundColor: '#0B1F34',
+    alignItems: 'center',
+  },
+  gamerImage: {
+    width: 309,
+    height: 288
+  },
+  title: {
+    color: '#00D4FF',
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginTop: 35,
+    fontFamily: "Play_700Bold",
+  },
+  subTitle: {
+    color: '#ED7947',
+    fontSize: 20,
+    marginTop: 20,
+    fontFamily: "Play_400Regular",
+  },
+  footer: {
+    marginTop: '20%',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#00D4FF',
+    flexDirection: 'row',
+    borderRadius: 20
+  },
+  buttonIcon: {
+    backgroundColor: '#ED7947',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontFamily: "Play_700Bold",
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#0B1F34',
+  }
+});
 
 export default Home;
