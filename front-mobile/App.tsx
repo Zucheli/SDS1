bd338dad12ced4dcebed272aff58ebd1932edcb8
Mateus Zucheli <mateuszucheli@hotmail.com>
2021-01-01 09:35:12 -0300

    Finalizando o App React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,9 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import { useFonts, Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
-import Header from './src/components/Header';
 import Routes from './src/routes';
 
 export default function App() {
